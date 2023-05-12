d5f32f45c0e448c1953b0011a72c97e94f3bd3cd
franydante <127851840+franydante@users.noreply.github.com>
2023-05-12 08:07:14 -0300

    Punto 1 y 2 terminado

---
+++ script1.js
@@ -0,0 +1,9 @@
+// Definir las variables con valores asignados
+var numero1 = 5;
+var numero2 = 10;
+
+// Realizar la suma
+var resultado = numero1 + numero2;
+
+// Mostrar el resultado por consola
+document.write(resultado);
---
+++ script2.js
@@ -0,0 +1,9 @@
+// Definir las variables con valores asignados
+var numero1 = 3;
+var numero2 = 9;
+
+// Realizar la multiplicacion
+var resultado = numero1 * numero2;
+
+// Mostrar el resultado por consola
+document.write(resultado);
